b94c5af5d598e607943f06cd192d483a8797d4f3
grupodot <rnunez@grupodot.com>
2019-07-08 09:32:40 -0500

    commit inicial

---
+++ pruebaramon/app/Http/Controllers/DataController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Data;
+use Illuminate\Http\Request;
+
+class DataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Data::all();
+        return json_encode($data);
+        
+    }
+}
